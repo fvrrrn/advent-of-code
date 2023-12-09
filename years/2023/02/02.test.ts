5ceb4833e8e56cea1584527a5744385aac43655e
fvrrrn <boris.chernystrand@gmail.com>
2023-12-09 19:08:21 +0300

    add 2023-2-2

--- years/2023/02/02.test.ts
+++ years/2023/02/02.test.ts
@@ -1,8 +1,8 @@
-import { p2023day2_part1 } from './02'
+import { p2023day2_part1, p2023day2_part2 } from './02'
 import * as fs from 'fs/promises'
 import * as path from 'path'
 
-test('Counts sum of sum of first and last digit in line 1', () => {
+test('p2023day2_part1 1', () => {
   expect(
     p2023day2_part1(
       `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
@@ -14,7 +14,24 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`,
   ).toBe(8)
 })
 
-test('Counts sum of sum of first and last digit in line 2', async () => {
+test('p2023day2_part1 2', async () => {
   const rawInput = await fs.readFile(path.join(__dirname, 'input.txt'), 'utf8')
   expect(p2023day2_part1(rawInput)).toBe(2727)
+})
+
+test('p2023day2_part2 1', () => {
+  expect(
+    p2023day2_part2(
+      `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`,
+    ),
+  ).toBe(2286)
+})
+
+test('p2023day2_part2 2', async () => {
+  const rawInput = await fs.readFile(path.join(__dirname, 'input.txt'), 'utf8')
+  expect(p2023day2_part2(rawInput)).toBe(56580)
 })
--- years/2023/02/02.ts
+++ years/2023/02/02.ts
@@ -23,3 +23,28 @@ export const p2023day2_part1 = (input: string): number => {
   }
   return result
 }
+
+export const p2023day2_part2 = (input: string): number => {
+  const processLine = (line: string): number => {
+    const colors = {
+      red: 0,
+      green: 0,
+      blue: 0,
+    }
+    const valuesRaw = line.split(':')[1]
+    if (!valuesRaw) return 0
+    for (const valueRaw of valuesRaw.split(';')) {
+      for (const cube of valueRaw.split(',')) {
+        const [_, count, color] = cube.split(' ')
+        if (count > colors[color]) colors[color] = Number.parseInt(count)
+      }
+    }
+    return colors.red * colors.blue * colors.green
+  }
+  let result = 0
+  const lines = input.split('\n')
+  for (const line of lines) {
+    result += processLine(line)
+  }
+  return result
+}
