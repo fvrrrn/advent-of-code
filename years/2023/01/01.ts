57dce5bd93dd0fc9b8dcfff64937791a4519d6af
fvrrrn <boris.chernystrand@gmail.com>
2023-12-09 14:20:15 +0300

    add 2023-1-2

--- years/2023/01/01.ts
+++ years/2023/01/01.ts
@@ -9,8 +9,33 @@ export const p2023day1_part1 = (input: string): number => {
 }
 
 export const p2023day1_part2 = (input: string): number => {
-  for (let i = 0; i < input.length; i++) {
-    //
+  const lines = input.split('\n')
+  let sum = 0
+  const fre =
+    /(\d)|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|(zero)/
+  const bre =
+    /(\d)|(eno)|(owt)|(eerht)|(ruof)|(evif)|(xis)|(neves)|(thgie)|(enin)|(orez)/
+  const english: { [key: string]: number } = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+    zero: 0,
   }
-  return 281
+  for (const line of lines) {
+    const first = fre.exec(line)?.[0]
+    const lastReversed = bre.exec([...line].reverse().join(''))?.[0]
+    if (first && lastReversed) {
+      const last = [...lastReversed].reverse().join('')
+      const firstVal = first.length > 1 ? english[first] : Number(first)
+      const lastVal = last.length > 1 ? english[last] : Number(last)
+      sum += Number(String(firstVal) + String(lastVal))
+    }
+  }
+  return sum
 }
